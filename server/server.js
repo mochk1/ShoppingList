42276ecdc19ae35fa9951bfe7bd15a982791b8ad
mochk1 <maor.wonderz@gmail.com>
2020-10-10 23:02:58 +0300

    .env

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 4000
+const port = process.env.PORT
 const routes = require('./routes.js')
 const cors = require('cors');
 var bodyParser = require('body-parser')
-
+require('dotenv').config();
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
