562db3cd324a2604fb63d8126bc4264a4730d004
mochk1 <maor.wonderz@gmail.com>
2020-10-11 04:32:09 +0300

    setup

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 require('dotenv').config();
-const port = process.env.PORT 
+const port = process.env.PORT || 8080
 const routes = require('./routes.js')
 const cors = require('cors');
 var bodyParser = require('body-parser')
+const path = require('path');
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -12,6 +13,16 @@ app.use(bodyParser.json())
 app.use(cors());
 app.use(express.json())
 app.use(routes);
+
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
 
 
 app.listen(port, () => console.log(`listening at http://localhost:${port}`))
