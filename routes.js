e2f75615989fd564c02a42a77c90c5c347a16595
mochk1 <maor.wonderz@gmail.com>
2020-10-12 23:18:11 +0300

    db load

--- routes.js
+++ routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const router = express.Router();
 const Datastore = require('nedb');
-const db = new Datastore({ filename: 'List.db' });
+const db = new Datastore({ filename: 'List.db',autoload:true });
 
 
   /* LOAD DB */
